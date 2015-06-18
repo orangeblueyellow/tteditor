a7288d8e83037d7a435f47d93f08b84ed3ba02ac
John Cote <jwcotejr@comcast.net>
2015-06-18 16:24:25 -0400

    Push the initial code

---
+++ direct/directbase/DirectStart.py
@@ -0,0 +1,19 @@
+try:
+    import libpandadx9
+except:
+    pass
+
+try:
+    import libpandadx8
+except:
+    pass
+
+try:
+    import libpandagl
+except:
+    pass
+
+print 'DirectStart: Starting the game.'
+
+from direct.showbase import ShowBase
+base = ShowBase.ShowBase()
